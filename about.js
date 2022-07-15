604fce61ef7088d089ef6b379996c39d0004c025
DoanAllie <doan.allie@gmail.com>
2022-07-15 09:29:41 -0500

    Part 2 completed

--- about.js
+++ about.js
@@ -4,11 +4,17 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	alert('form submit')
+	// console.log('form submit');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let image = document.querySelector('content img')
+function alertCompliment() {
+	alert('You have a heart of gold.')
+}
+image.addEventListener('mouseover', alertCompliment)
